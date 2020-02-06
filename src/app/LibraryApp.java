58f187472d672101a95a85ccac22dc9e10d0cb4d
piotrszacilowski <szacilowski.piotr@gmail.com>
2020-02-06 15:04:47 +0100

    Changing appName variable to final static

--- src/app/LibraryApp.java
+++ src/app/LibraryApp.java
@@ -1,9 +1,10 @@
 package app;
 
 class LibraryApp {
+    private static final String APP_NAME = "Biblioteka v0.9";
+
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.9";
-        System.out.println(appName);
+        System.out.println(APP_NAME);
 
         LibraryControl libraryControl = new LibraryControl();
         libraryControl.controlLoop();
