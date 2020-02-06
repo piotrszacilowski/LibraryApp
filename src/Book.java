9b690a2d4c6ee846c54467be1adcd5cb456201e9
piotrszacilowski <szacilowski.piotr@gmail.com>
2020-02-06 13:10:44 +0100

    Added private access modifiers to book fields

--- src/Book.java
+++ src/Book.java
@@ -1,10 +1,10 @@
 public class Book {
-    String title;
-    String author;
-    int releaseDate;
-    int pages;
-    String publisher;
-    String isbn;
+    private String title;
+    private String author;
+    private int releaseDate;
+    private int pages;
+    private String publisher;
+    private String isbn;
 
     public Book(String title, String author, int releaseDate, int pages, String publisher, String isbn) {
         this(title, author, releaseDate, pages, publisher);
@@ -17,6 +17,54 @@ public class Book {
         this.releaseDate = releaseDate;
         this.pages = pages;
         this.publisher = publisher;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(int releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
     }
 
     void printInfo() {
