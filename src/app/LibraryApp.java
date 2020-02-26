98c438f1de52f25eec648b13ea4516987b0fec1c
Piotr Szaciłowski <szacilowski.piotr@gmail.com>
2020-02-26 15:55:55 +0100

    Updated version number of application

--- src/app/LibraryApp.java
+++ src/app/LibraryApp.java
@@ -1,7 +1,7 @@
 package app;
 
 class LibraryApp {
-    private static final String APP_NAME = "Biblioteka v1.2";
+    private static final String APP_NAME = "Biblioteka v1.3";
 
     public static void main(String[] args) {
         System.out.println(APP_NAME);
